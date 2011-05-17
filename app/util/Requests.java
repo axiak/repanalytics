23ab1d36cc7bb20f30e42b499e33a428f9a371ab
Michael Axiak <maxiak@crunchtime.com>
2011-05-17 23:24:18 +0000

    DI32262 - Meridian - Added logging around the ipaddress determination.
    git-svn-id: http://svn.crunchtime.local/repmanager/trunk@28 76ca42cc-28e3-4466-93a6-d0125720c398

--- app/util/Requests.java
+++ app/util/Requests.java
@@ -1,5 +1,6 @@
 package util;
 
+import play.Logger;
 import play.Play;
 import play.mvc.Http;
 
@@ -15,6 +16,7 @@ public final class Requests {
         if (ip.startsWith("127.") || ip.startsWith("192.168.")) {
             ip = Play.configuration.getProperty("test.ip.address", "209.113.164.2");
         }
+        Logger.info("Remote IP Address: %s", ip);
         return ip;
     }
 }
