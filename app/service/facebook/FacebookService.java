c7fd39be317afbf9908a32213886c3ce5523d50f
Michael Axiak <maxiak@crunchtime.com>
2011-05-19 01:12:05 +0000

    DI32262 - Meridian - Changed yelp api keys.
    git-svn-id: http://svn.crunchtime.local/repmanager/trunk@38 76ca42cc-28e3-4466-93a6-d0125720c398

---
+++ app/service/facebook/FacebookService.java
@@ -0,0 +1,11 @@
+package service.facebook;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: axiak
+ * Date: 5/18/11
+ * Time: 8:25 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FacebookService {
+}
---
+++ app/service/search/PolyRemoteServiceSearchBuilder.java
@@ -0,0 +1,11 @@
+package service.search;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: axiak
+ * Date: 5/18/11
+ * Time: 8:32 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PolyRemoteServiceSearchBuilder {
+}
